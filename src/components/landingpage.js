8d7e049c10e08d2c2e7a1672c1989082843cfc59
Jullia Tran <RainbowUnicorn@Julias-MacBook-Pro-2.local>
2019-07-26 11:04:49 +0700

    initial commit

---
+++ src/components/landingpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Resume extends React.Component {
+  render() {
+    return (
+      <div> 
+        <p>Landing Page</p>
+      </div>
+    );
+  }
+}
+export default Resume;
