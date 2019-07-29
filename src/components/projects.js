671077293f62b210397027de1a01921cf735f2f6
Jullia Tran <RainbowUnicorn@Jullia-MacBook-Pro.local>
2019-07-29 21:32:58 +0700

    Fixed landing page

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,7 +9,23 @@ class Project extends Component {
   toggleCategories() {
     if(this.state.activeTab === 0) {
       return (
-        <div><h1>This is Python</h1></div>
+        <div><h1>This is AngleHack 2019</h1></div>
+      )
+    } else if(this.state.activeTab === 1) {
+      return (
+        <div><h1>This is Machine Learning</h1></div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return (
+        <div><h1>This is Webscraper</h1></div>
+      )
+    } else if(this.state.activeTab === 3) {
+      return (
+        <div><h1>This is RubiksPaint</h1></div>
+      )
+    } else if(this.state.activeTab === 4) {
+      return (
+        <div><h1>This is Hack112</h1></div>
       )
     }
   }
@@ -18,10 +34,11 @@ class Project extends Component {
     return (
       <div className="category-tabs">
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
-          <Tab>Python</Tab>
+          <Tab>AngelHack 2019</Tab>
           <Tab>Machine Learning</Tab>
           <Tab>Webscraper</Tab>
-          <Tab>AngelHack 2019</Tab>
+          <Tab>RubiksPaint</Tab>
+          <Tab>Hack112</Tab>
         </Tabs>
       </div>
     );
