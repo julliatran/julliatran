75c271f944ff575c03300ef4bd42b8ceb3d3b62a
Jullia Tran <utran@andrew.cmu.edu>
2019-08-12 02:11:01 +0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList } from 'react-mdl';
 import Main from'./components/main';
 import { Link } from 'react-router-dom';
 function App() {
@@ -27,6 +27,16 @@ function App() {
         <div className="page-content" />
         <Main/>
       </Content>
+      <Footer className="footer" size="mega">
+        <FooterSection type="top">
+          <FooterLinkList>
+            <div style={{textAlign:'center'}}>
+              <h4><strong>Contact me here! </strong></h4>
+              <p><strong>tranjullia@gmail.com</strong></p>
+            </div>
+          </FooterLinkList>
+        </FooterSection>
+      </Footer>
     </Layout>
   </div>
   );
